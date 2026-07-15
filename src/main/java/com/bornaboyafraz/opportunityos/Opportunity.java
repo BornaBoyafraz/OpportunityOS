e88f20d46f3d274fd6258e65ed1db4e6502149e3
Borna Afraz <bornaboyafraz@gmail.com>
2026-07-14 21:26:19 -0400

    Add Opportunity model

---
+++ src/main/java/com/bornaboyafraz/opportunityos/Opportunity.java
@@ -0,0 +1,39 @@
+package com.bornaboyafraz.opportunityos;
+
+public class Opportunity {
+    
+    // Variables
+    String company, position, status;
+    String deadline; // Will be changed
+
+
+    // Functions
+    public void printl(Object massage){
+        System.out.println(massage);
+    }
+
+    public void print(Object massage){
+        System.out.print(massage);
+    }
+    
+    // Constructor
+    public Opportunity(String company, String position, String status, String deadline){
+        this.company = company;
+        this.position = position;
+        this.status = status;
+        this.deadline = deadline;
+
+    }
+    public void describe(){ // a function to describe the positions fully
+        printl(company+ " company || "+ position + " position ||" + status + " status ||" + deadline + " deadline");
+    }
+
+
+    public static void main(String[] args) {
+        
+        Opportunity myOpportunity = new Opportunity("Linkdine", "Software Endineer", "Hiring", "Monday");
+        myOpportunity.describe();
+
+    }
+
+}
