5e463f39847bb9a9699002a5c257f38b1c1df002
Borna Afraz <bornaboyafraz@gmail.com>
2026-07-16 00:30:53 -0400

    Disable data source auto-configuration

--- src/main/java/com/bornaboyafraz/opportunityos/OpportunityosApplication.java
+++ src/main/java/com/bornaboyafraz/opportunityos/OpportunityosApplication.java
@@ -2,8 +2,9 @@ package com.bornaboyafraz.opportunityos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = {DataSourceAutoConfiguration.class})
 public class OpportunityosApplication {
 
 	public static void main(String[] args) {
