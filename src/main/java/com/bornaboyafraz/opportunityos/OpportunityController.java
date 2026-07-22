9a148ddb755f190e923b8dba46d774f3bdbd5bd7
Borna Afraz <bornaboyafraz@gmail.com>
2026-07-22 12:35:07 -0400

    feat: add repository and POST endpoint to save opportunities

--- src/main/java/com/bornaboyafraz/opportunityos/OpportunityController.java
+++ src/main/java/com/bornaboyafraz/opportunityos/OpportunityController.java
@@ -3,11 +3,22 @@ package com.bornaboyafraz.opportunityos;
 import java.time.LocalDate;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 public class OpportunityController {
+
+    //the field
+    private final OpportunityRepository repository;
+
+    //the constructor
+    public OpportunityController(OpportunityRepository repository){
+        this.repository = repository;
+    }
+
+    // endpoint methods
     @GetMapping("/opportunities")
     public List<Opportunity> gOpportunities(){
         
@@ -20,5 +31,14 @@ public class OpportunityController {
 
         return opportunitiesList;
     }
+
+    @PostMapping("/opportunities")
+    public Opportunity addOpportunity(@RequestBody Opportunity opportunity) {
+
+        return repository.save(opportunity);
+    }
+
+
+
     
 }
---
+++ src/main/java/com/bornaboyafraz/opportunityos/OpportunityRepository.java
@@ -0,0 +1,7 @@
+package com.bornaboyafraz.opportunityos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OpportunityRepository extends JpaRepository<Opportunity, Long> {
+
+}
