3f8531b1c34be72388a37fdf58c58e1672fde6bd
Borna Afraz <bornaboyafraz@gmail.com>
2026-07-22 22:06:18 -0400

    feat: fetch opportunities from repository

--- src/main/java/com/bornaboyafraz/opportunityos/OpportunityController.java
+++ src/main/java/com/bornaboyafraz/opportunityos/OpportunityController.java
@@ -1,6 +1,5 @@
 package com.bornaboyafraz.opportunityos;
 
-import java.time.LocalDate;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,15 +20,12 @@ public class OpportunityController {
     // endpoint methods
     @GetMapping("/opportunities")
     public List<Opportunity> gOpportunities(){
-        
 
-        Opportunity opp1 = new Opportunity("Google", "SWE Intern", "Applied", LocalDate.of(2026, 6, 20), "https://google.com/careers");
-        //Opportunity opp2 = new Opportunity("Amazon", "Backend Intern", "Interested", "Saturday");
+        // Opportunity opp1 = new Opportunity("Google", "SWE Intern", "Applied", LocalDate.of(2026, 6, 20), "https://google.com/careers");
+        // //Opportunity opp2 = new Opportunity("Amazon", "Backend Intern", "Interested", "Saturday");
 
-
-        List<Opportunity> opportunitiesList = List.of(opp1);
-
-        return opportunitiesList;
+        // List<Opportunity> opportunitiesList = List.of(opp1);
+        return repository.findAll();
     }
 
     @PostMapping("/opportunities")
