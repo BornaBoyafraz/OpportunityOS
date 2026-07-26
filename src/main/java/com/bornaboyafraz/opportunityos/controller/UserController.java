9fa1ce8782dfe27e68666311cdae2dd91067b2a2
Borna Afraz <bornaboyafraz@gmail.com>
2026-07-26 12:08:31 -0400

    feat: add login endpoint verifying password against stored hash

--- src/main/java/com/bornaboyafraz/opportunityos/controller/UserController.java
+++ src/main/java/com/bornaboyafraz/opportunityos/controller/UserController.java
@@ -1,8 +1,8 @@
 package com.bornaboyafraz.opportunityos.controller;
 import com.bornaboyafraz.opportunityos.model.User;
 import com.bornaboyafraz.opportunityos.repository.UserRepository;
-
 import jakarta.validation.Valid;
+import java.util.Optional;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,5 +25,18 @@ public class UserController {
         user.setPassword(encoder.encode(user.getPassword()));
         return repository.save(user);
     }
+
+    @PostMapping("login")
+    public String login(@RequestBody User loginRequest) {
+        
+        Optional<User> found = repository.findByUsername(loginRequest.getUsername());
+
+        if (found.isPresent() && encoder.matches(loginRequest.getPassword(), found.get().getPassword())){
+            return "Login successful";
+        }
+        
+        return "Invalid username or password";
+    }
+    
     
 }
